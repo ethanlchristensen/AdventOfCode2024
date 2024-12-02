a84021565dc2286e1931e230664922f6d6b7f5c2
ethanlchristensen <ethanlchristensen@outlook.com>
2024-12-02 14:38:39 -0600

    [update] - update main.py to also have an --up-to-today flag to see solutions to all problems up to the current day

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import os
 import argparse
 from datetime import datetime
 
-# set up argument parsing
+# Set up argument parsing
 parser = argparse.ArgumentParser(description="Run Advent of Code problems.")
 parser.add_argument("--all", action="store_true", help="Run all problems")
+parser.add_argument("--up-to-today", action="store_true", help="Run all problems up to and including the current day")
 args = parser.parse_args()
 
 main_path = os.getcwd() + "\\%s"
@@ -17,16 +18,26 @@ current_month = current_date.month
 folders = list(dict(sorted({folder_name: int(folder_name.split('-')[0]) for folder_name in os.listdir(
     main_path % '') if re.match(r'\d*\-day\-.*', folder_name)}.items(), key=lambda x: x[1])).keys())
 
-# determine whether to run only today's problem or all of them
-if not args.all and current_month == 12:
-    folders = [folder for folder in folders if int(folder.split('-')[0]) == current_day]
+# determine which problems to run based on flags
+if not args.all:
+    if current_month == 12:
+        if args.up_to_today:
+            # run all problems up to the current day
+            folders = [folder for folder in folders if int(folder.split('-')[0]) <= current_day]
+        else:
+            # run only the problem of the current day
+            folders = [folder for folder in folders if int(folder.split('-')[0]) == current_day]
 
-# run the solutions
 for folder in folders:
     print(f"{re.sub(r'[^A-Z ]', '', folder.replace('-', ' ').upper()).strip():=^35s}")
+    # solution file from the folder
     module = __import__(folder, fromlist=[folder])
+    # get the file as module
     day = getattr(module, folder)
+    # change dir to that folder (to access data)
     os.chdir(main_path % folder)
+    # invoke the solve command for the module
     day.solve()
+    # change dir back to the main path
     os.chdir(main_path % '')
     print()
