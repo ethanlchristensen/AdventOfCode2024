0dfbc39bbb95393877e988a2f8cff6e598ed201a
ethanlchristensen <ethanlchristensen@outlook.com>
2024-12-02 11:58:30 -0600

    Day Two Solutions

--- 2-day-two/2-day-two.py
+++ 2-day-two/2-day-two.py
@@ -5,20 +5,46 @@ def load_data(name='data'):
     file = open(name, 'r')
     data = [line.strip() for line in file.readlines()]
     file.close()
+    for idx in range(len(data)):
+        data[idx] = [int(v) for v in data[idx].split(" ")]
     return data
-    
+
+def is_safe(report: list[int]) -> bool:
+        s = sorted(report)
+        if s == report or s[::-1] == report:
+            for idx in range(len(report) - 1):
+                v = abs(report[idx] - report[idx + 1])
+                if not (v >= 1 and v <= 3):
+                    return False
+        else:
+            return False
+        return True
     
 def part_one():
     """
     code to solve part one
     """
-    return None
+    reports = load_data()
+    safe = 0
+    for report in reports:
+        if is_safe(report): safe += 1
+    return safe
 
 def part_two():
     """
     code to solve part two
     """
-    return None
+    reports = load_data()
+    safe = 0
+    for report in reports:
+        if is_safe(report): safe += 1
+        else:
+            for idx in range(len(report)):
+                new_report = [v for i, v in enumerate(report) if i != idx]
+                if is_safe(new_report):
+                    safe += 1
+                    break
+    return safe
     
 def solve():
     """
