77e72efd8351c13f82f4a5b8d40454b5d6c30c15
ethanlchristensen <ethanlchristensen@outlook.com>
2024-12-02 20:03:28 -0600

    [update] - will make one line later for day 2

--- 2-day-two/2-day-two.py
+++ 2-day-two/2-day-two.py
@@ -1,6 +1,3 @@
-
-import re
-
 def load_data(name='data'):
     file = open(name, 'r')
     data = [line.strip() for line in file.readlines()]
@@ -9,17 +6,20 @@ def load_data(name='data'):
         data[idx] = [int(v) for v in data[idx].split(" ")]
     return data
 
+def is_safe(report: list[int]) -> bool:
+        return False if not any([report == sorted(report), report == sorted(report)[::-1]]) else all([(abs(report[idx] - report[idx + 1]) >= 1 and abs(report[idx] - report[idx + 1]) <= 3) for idx in range(len(report) - 1)])
+    
 def part_one():
     """
     code to solve part one
     """
-    return sum([1 if all([(report == sorted(report) or report == sorted(report)), all([abs(report[idx] - report[idx + 1]) >= 1 and abs(report[idx] - report[idx + 1]) <= 3]) for idx in range(len(report) - 1)]) else 0 for report in load_data()])
+    return sum([1 if (is_safe(report)) else 0 for report in load_data()])
 
 def part_two():
     """
     code to solve part two
     """
-    return sum([1 if (all([(report == sorted(report) or report == sorted(report)), all([abs(report[idx] - report[idx + 1]) >= 1 and abs(report[idx] - report[idx + 1]) <= 3]) for idx in range(len(report) - 1)]) or any([is_safe([v for i, v in enumerate(report) if i != idx]) for idx in range(len(report))])) else 0 for report in load_data()])
+    return sum([1 if (is_safe(report) or any([is_safe([v for i, v in enumerate(report) if i != idx]) for idx in range(len(report))])) else 0 for report in load_data()])
     
 def solve():
     """
