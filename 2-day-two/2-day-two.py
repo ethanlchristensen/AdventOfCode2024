2ccf1a0db4c37962c9811cbef7cdd96d249b10e3
ethanlchristensen <ethanlchristensen@outlook.com>
2024-12-03 21:34:03 -0600

    [update] - made day 2 solutions oneliner

--- 2-day-two/2-day-two.py
+++ 2-day-two/2-day-two.py
@@ -6,21 +6,17 @@ def load_data(name='data'):
         data[idx] = [int(v) for v in data[idx].split(" ")]
     return data
 
-def is_safe(report: list[int]) -> bool:
-        return False if not any([report == sorted(report), report == sorted(report)[::-1]]) else all([(abs(report[idx] - report[idx + 1]) >= 1 and abs(report[idx] - report[idx + 1]) <= 3) for idx in range(len(report) - 1)])
-    
 def part_one():
     """
     code to solve part one
     """
-    return sum([1 if (is_safe(report)) else 0 for report in load_data()])
+    return sum([1 if (False if not any([report == sorted(report), report == sorted(report)[::-1]]) else all([(abs(report[idx] - report[idx + 1]) >= 1 and abs(report[idx] - report[idx + 1]) <= 3) for idx in range(len(report) - 1)])) else 0 for report in load_data()])
 
 def part_two():
     """
     code to solve part two
     """
-    return sum([1 if (is_safe(report) or any([is_safe([v for i, v in enumerate(report) if i != idx]) for idx in range(len(report))])) else 0 for report in load_data()])
-    
+    return sum([1 if ((False if not any([report == sorted(report), report == sorted(report)[::-1]]) else all([(abs(report[idx] - report[idx + 1]) >= 1 and abs(report[idx] - report[idx + 1]) <= 3) for idx in range(len(report) - 1)])) or any([(False if not any([[v for i, v in enumerate(report) if i != idx] == sorted([v for i, v in enumerate(report) if i != idx]), [v for i, v in enumerate(report) if i != idx] == sorted([v for i, v in enumerate(report) if i != idx])[::-1]]) else all([(abs([v for i, v in enumerate(report) if i != idx][j] - [v for i, v in enumerate(report) if i != idx][j + 1]) >= 1 and abs([v for i, v in enumerate(report) if i != idx][j] - [v for i, v in enumerate(report) if i != idx][j + 1]) <= 3) for j in range(len([v for i, v in enumerate(report) if i != idx]) - 1)])) for idx in range(len(report))])) else 0 for report in load_data()])    
 def solve():
     """
     code to run part one and part two
--- 3-day-three/3-day-three.py
+++ 3-day-three/3-day-three.py
@@ -12,13 +12,13 @@ def part_one():
     """
     code to solve part one
     """
-    return sum([int(match[0]) * int(match[1]) for match in re.findall(pattern=r'mul\((\d+),(\d+)\)', string=load_data())])
+    return sum(int(match[0]) * int(match[1]) for match in re.findall(pattern=r'mul\((\d+),(\d+)\)', string=load_data()))
 
 def part_two():
     """
     code to solve part two
     """
-    data=[v.group() for v in list(re.finditer(pattern=r'mul\((\d+),(\d+)\)|do\(\)|don\'t\(\)', string=load_data()))];return sum([(int(re.search("mul\((?P<a>\d+),(?P<b>\d+)\)", mul).groups()[0]) * int(re.search("mul\((?P<a>\d+),(?P<b>\d+)\)", mul).groups()[1])) for mul in [item for i, item in enumerate(data) if data[i] != "do()" and data[i] != "don't()" and (i < next((idx for idx, val in enumerate(data) if val == "don't()"), len(data)) or any(j < i < k for j in (idx for idx, val in enumerate(data) if val == "do()") for k in [next((idx for idx, val in enumerate(data[j:], j) if val == "don't()"), len(data))]))]])
+    data=[v.group() for v in list(re.finditer(pattern=r'mul\((\d+),(\d+)\)|do\(\)|don\'t\(\)', string=load_data()))];return sum((int(re.search("mul\((?P<a>\d+),(?P<b>\d+)\)", mul).groups()[0]) * int(re.search("mul\((?P<a>\d+),(?P<b>\d+)\)", mul).groups()[1])) for mul in [item for i, item in enumerate(data) if data[i] != "do()" and data[i] != "don't()" and (i < next((idx for idx, val in enumerate(data) if val == "don't()"), len(data)) or any(j < i < k for j in (idx for idx, val in enumerate(data) if val == "do()") for k in [next((idx for idx, val in enumerate(data[j:], j) if val == "don't()"), len(data))]))])
     
 def solve():
     """
